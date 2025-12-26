d36e65530db4e3e1f1f255951a7a162ad1ecc7a1
Spectra010s <adeloyeadetayo8@gmail.com>
2025-12-26 15:27:05 +0100

    fix: update layout.tsx typo error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,23 @@
-import type React from 'react';
-import type { Metadata, Viewport } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
-import { Analytics } from '@vercel/analytics/next';
-import ThemeProvider from '@/context/theme-provider';
-import ThemeMeta from '@/components/ThemeMeta';
+import type React from "react"
+import type { Metadata, Viewport } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+import { Analytics } from '@vercel/analytics/next'
+import ThemeProvider from '@/context/theme-provider'
+import ThemeMeta from '@/components/ThemeMeta'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
-});
+})
 
-const APP_DEFAULT_TITLE = 'Blend • Multitasking made easy';
+const APP_DEFAULT_TITLE = 'Blend • Multitasking made easy'
+
 
 export const metadata: Metadata = {
   title: APP_DEFAULT_TITLE,
@@ -29,7 +30,7 @@ export const metadata: Metadata = {
     title: APP_DEFAULT_TITLE,
     description: 'Blend is an app that provides you with tools used for multitasking.',
     siteName: 'Blend',
-    type: 'website',
+    type: "website",
   },
   twitter: {
     card: 'summary_large_image',
@@ -38,22 +39,22 @@ export const metadata: Metadata = {
   },
   appleWebApp: {
     capable: true,
-    statusBarStyle: 'default',
+    statusBarStyle: "default",
     title: APP_DEFAULT_TITLE,
     // startUpImage: [],
   },
   formatDetection: {
     telephone: false,
   },
-};
+}
 
 export const viewport: Viewport = {
-  themeColor: '#FFFFFF',
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" dur="ltr">
+    <html lang="en" dir="ltr">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider>
           <ThemeMeta />
@@ -62,5 +63,5 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Analytics />
       </body>
     </html>
-  );
+  )
 }
