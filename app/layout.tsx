d8bf1186c3ba4979a6c22269e16b03d107d9dd4c
Spectra010s <adeloyeadetayo8@gmail.com>
2026-01-04 07:36:35 +0100

    fix: layout of offline page and code cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,22 @@
-import type React from "react"
-import type { Metadata, Viewport } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
-import { Analytics } from '@vercel/analytics/next'
-import ThemeProvider from '@/context/theme-provider'
-import ThemeMeta from '@/components/ThemeMeta'
+import type React from 'react';
+import type { Metadata, Viewport } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { Analytics } from '@vercel/analytics/next';
+import ThemeProvider from '@/context/theme-provider';
+import ThemeMeta from '@/components/ThemeMeta';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
-})
+});
 
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
-})
+});
 
-const APP_DEFAULT_TITLE = 'Blend • Multitasking made easy'
-
+const APP_DEFAULT_TITLE = 'Blend • Multitasking made easy';
 
 export const metadata: Metadata = {
   title: APP_DEFAULT_TITLE,
@@ -30,7 +29,7 @@ export const metadata: Metadata = {
     title: APP_DEFAULT_TITLE,
     description: 'Blend is an app that provides you with tools used for multitasking.',
     siteName: 'Blend',
-    type: "website",
+    type: 'website',
   },
   twitter: {
     card: 'summary_large_image',
@@ -39,17 +38,17 @@ export const metadata: Metadata = {
   },
   appleWebApp: {
     capable: true,
-    statusBarStyle: "default",
+    statusBarStyle: 'default',
     title: APP_DEFAULT_TITLE,
     // startUpImage: [],
   },
   formatDetection: {
     telephone: false,
   },
-}
+};
 
 export const viewport: Viewport = {
-  themeColor: "#FFFFFF",
+  themeColor: '#FFFFFF',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -63,5 +62,5 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
--- app/~offline/page.tsx
+++ app/~offline/page.tsx
@@ -1,9 +1,15 @@
+'use client';
+import { Button } from '@/components/ui/button';
+
 export default function Offline() {
   return (
-    <div style={{ textAlign: 'center', padding: '50px' }}>
-      <h1>You are currently offline</h1>
-      <p>Please check your internet connection and try again.</p>
-      <button onClick={() => window.location.reload()}>Retry</button>
-    </div>
+    <main className=" min-h-screen flex flex-col justify-center items-center gap-4 ">
+        <h1>You are currently offline</h1>
+        <p>Please check your internet connection and try again.</p>
+        <Button variant="default" onClick={() => window.location.reload()}>
+          Retry
+        </Button>
+
+    </main>
   );
 }
